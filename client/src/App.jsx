5417e038e7816d312fa9ee78657e40d56722a6ac
NewGitUser674 <02mrjon02@gmail.com>
2026-03-05 19:51:06 -0500

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Chat from "./Chat";
+
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>ChatGPT Clone</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
